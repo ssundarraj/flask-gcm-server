21e3b7d2761309b7c4ee7789edb71e67eae4c256
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-02 03:08:56 +0530

    Renamed regs.py to device.py

--- app/models/dbutils.py
+++ app/models/dbutils.py
@@ -1,9 +1,9 @@
 from .. import db
-import regs
+import device
 
 
 def add_new_device(reg_id, status):
-    new_user = regs.Device(reg_id, status)
+    new_user = device.Device(reg_id, status)
     db.session.add(new_user)
     db.session.commit()
     return new_user.id
