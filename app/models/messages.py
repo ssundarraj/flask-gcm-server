2895dda6ffc2eab26e6ca1587975f74e35d506c1
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-02 03:12:27 +0530

    Moved device to devices. Added messsage model.

---
+++ app/models/messages.py
@@ -0,0 +1,12 @@
+from .. import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message_text = db.Column(db.String(1024), index=True, unique=True)
+
+    def __init__(self, message_text):
+        self.message_text = message_text
+
+    def __repr__(self):
+        return '<Message {0}>'.format(self.id)
