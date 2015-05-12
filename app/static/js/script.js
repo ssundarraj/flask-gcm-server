1d9394a424586ccb5c732a470b2ab7a8badb3a4a
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-12 21:44:52 +0530

    Added AJAX calling to add message.

---
+++ app/static/js/script.js
@@ -0,0 +1,26 @@
+function sendNotifData(){
+    var msgString = document.getElementById("notif_msg").value;
+    if(msgString != ""){
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp=new XMLHttpRequest();
+        }else {  // code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                if(xmlhttp.responseText != "0") {
+                    //success
+                    console.log(xmlhttp.responseText);
+                }
+                else {
+                    //failure
+                    console.log(xmlhttp.responseText);
+                }
+            }
+        }
+        xmlhttp.open("POST","./",true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("notif_msg="+msgString);
+    }
+}
